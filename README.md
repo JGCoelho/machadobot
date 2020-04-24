60255b2aa295ed0dff23327fdfc25fa8f8d40fd8
JGCoelho <josegustavocoelho@gmail.com>
2020-04-23 23:36:54 -0300

    fixed?

---
+++ README.md
@@ -0,0 +1,16 @@
+You will need to create an file named config.json with the following content:
+
+```json
+{
+    "MACHADO":{
+        "consumer_key" : "CONSUMER KEY",
+        "consumer_secret" : "CONSUMER SECRET",
+        "access_token_key" : "TOKEN KEY",
+        "access_token_secret" : "TOKEN SECRET",
+        "database" : "db.db",
+        "model": "models\\default.json"
+    }
+}
+```
+
+Put your credentials in the fields and leave the file in the folder.
--- machado.py
+++ machado.py
@@ -1,5 +1,5 @@
 import markovify, nltk, datetime, time, twitter, json, sqlite3
-import logging 
+import re, random, logging 
 logging.basicConfig(filename="log.log", level=logging.INFO,
 					filemode="w")
 
@@ -9,12 +9,17 @@ with open("config.json") as json_data_file:
 	data = json.load(json_data_file)
 
 
-CONSUMER_KEY = data["MACHADO"]["consumer_key"]
-CONSUMER_SECRET= data["MACHADO"]["consumer_secret"]
-ACCESS_TOKEN_KEY= data["MACHADO"]["access_token_key"]
-ACCESS_TOKEN_SECRET= data["MACHADO"]["access_token_secret"]
-DB = data["MACHADO"]["database"]
-
+CONSUMER_KEY = data["SETTINGS"]["consumer_key"]
+CONSUMER_SECRET= data["SETTINGS"]["consumer_secret"]
+ACCESS_TOKEN_KEY= data["SETTINGS"]["access_token_key"]
+ACCESS_TOKEN_SECRET= data["SETTINGS"]["access_token_secret"]
+DB = data["SETTINGS"]["database"]
+MODELS = data["SETTINGS"]["models"]
+FULL_NAME = {
+	"Machado" : "Machado de Assis",
+	"Sabino" : "Fernando Sabino",
+	"Guimaraes" : "Guimarães Rosa"
+}
 
 def login():
 	api = twitter.Api(consumer_key= CONSUMER_KEY,
@@ -65,21 +70,75 @@ class Database:
 
 def save_model_as_json(text_model, file_name):
 	model_json = text_model.to_json()
-	with open('models\\' + file_name + 'json', 'w') as outfile:
+	with open('models\\' + file_name + '.json', 'w') as outfile:
 		json.dump(model_json, outfile)
 
 def load_model_from_json(file_name):
-	with open('models\\' + file_name + 'json') as json_file:
+	with open('models\\' + file_name + '.json') as json_file:
 		return markovify.Text.from_json(json.load(json_file))
 
 def get_machado():
-	return load_model_from_json('default')
+	return load_model_from_json('Machado')
 
 def get_sabino():
-	return load_model_from_json('sabino')
+	return load_model_from_json('Sabino')
+
+def get_guimaraes():
+	return load_model_from_json('Rosa')
 
 def model_and_save(file_name):#also returns the model
-	with open('samples\\' + file_name + '.txt', encoding = 'utf-8') as file:
+	with open('samples\\' + file_name + '.txt') as file:
 		model = markovify.Text(file.read(), state_size = 3)
 	save_model_as_json(model, file_name)
 	return model
+
+
+################################################################
+#Methods for logging, using the api and making posts.
+################################################################
+flawed_utf_pattern = re.compile('\\.*')
+quote_pattern = re.compile("\"")
+
+def make_post(content):
+	pass#nothing for now
+
+def choose_model():
+	model_name = random.choice(MODELS)
+	model = load_model_from_json(model_name)
+	return model, model_name
+
+def make_a_message(model, model_name):
+	
+	min_size = 200
+	max_size = 240
+	entire_sentence = ""
+	
+	while len(entire_sentence) < min_size:
+		if len(entire_sentence) != 0:
+			entire_sentence += " "
+		sentence = None
+		while sentence == None:
+			sentence = model.make_short_sentence(max_size - len(entire_sentence))
+
+		#the machado model has some accented
+		#characters that for some reason don't
+		#get formated nicely. In this step
+		#we remove them and do some more formatting
+
+		sentence = re.sub(flawed_utf_pattern, "", sentence)
+		sentence = re.sub(quote_pattern,"", sentence)
+		if sentence[-1] == ",":
+			sentence = sentence[:-1] + "."
+		elif sentence[-1] not in ["?","!",".","\""]:
+			sentence = sentence + "."
+		entire_sentence += sentence 
+
+	message = "{}: \"{}\"".format(FULL_NAME[model_name], entire_sentence)
+	return message
+
+def test_each_model():
+	for model_name in MODELS:
+		model = load_model_from_json(model_name)
+		print(make_a_message(model, model_name))
+
+test_each_model()
